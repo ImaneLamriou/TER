87a64d88ab80ccdb19e6a505d974fc288adb26d0
Amir SHIRALI POUR <emasalari@gmail.com>
2020-02-25 03:19:15 +0100

    linksOfpagellapolitica() is finished

--- library/extraction.py
+++ library/extraction.py
@@ -14,14 +14,13 @@ def linksOfpagellapolitica():
     uri = 'dichiarazioni/verificato?page='
     # for i in range(sys.maxsize):
     newArticles = {}
-    articleNumbers = 1
+    totalNumberOfArticles = 1
     for i in range(sys.maxsize):
         pageNumber = i
         pageNumber = str(pageNumber)
         address = url + uri + pageNumber
 
         resp = requests.get(address)
-
         if resp:
             source = requests.get(address).text
             soup = bs.BeautifulSoup(source, "lxml")
@@ -34,28 +33,32 @@ def linksOfpagellapolitica():
                 if table.find('div', {'class': 'mb-0 px-2 min-height-title'}):
                     div = table.find(
                         'div', {'class': 'mb-0 px-2 min-height-title'})
-                    title = div.find('span', {'class': 'h6'}).text
+                    title = div.find('span', {'class': 'h6'}).text.strip()
                 elif table.find('div', {'class': 'mb-2 mt-2 px-2'}):
                     div = table.find('div', {'class': 'mb-2 mt-2 px-2'})
 
                     if div.find('span', {'class': 'h2'}):
-                        title = div.find('span', {'class': 'h2'}).text
+                        title = div.find('span', {'class': 'h2'}).text.strip()
                 elif table.find('article', {'class': 'article'}):
                     article = table.find('article', {'class': 'article'})
                     p = article.find('p', {'class': 'font-size-15'})
                     if p.find('span', {'class': 'text-dark'}):
                         title = p.find(
-                            'span', {'class': 'text-dark'}).text
+                            'span', {'class': 'text-dark'}).text.strip()
 
                 link = table.find('a', {'class': 'statement-link'})['href']
                 idPattern = re.search(r'/([0-9]+)/', link)
                 idNumber = idPattern.group(1)
-                # print("Article\n = Link:", link, "\nTitle:", title)
                 newArticles[idNumber] = {'link': link,
                                          'title': title
                                          }
-                print(articleNumbers, idNumber, title)
-                articleNumbers += 1
+                print(totalNumberOfArticles, idNumber, title)
+                totalNumberOfArticles += 1
+
+                if idNumber == str(96):
+                    break
+        if idNumber == str(96):
+            break
 
     return newArticles
 
@@ -64,15 +67,13 @@ def writeLinksJson():
     file = 'dataBase/newArticlesLinks.json'
     newArticles = linksOfpagellapolitica()
     with open(file, 'w') as jsonOut:
-        json.dump(newArticles, jsonOut, indent=2)
+        json.dump(newArticles, jsonOut, indent=2, ensure_ascii=False)
 
 
-# writeLinksJson()
+writeLinksJson()
+
+
 def readLinksJson():
     file = 'dataBase/newArticlesLinks.json'
     with open(file) as jsonIn:
         json.load(jsonIn)
-
-
-data = readLinksJson()
-print(len(data))
