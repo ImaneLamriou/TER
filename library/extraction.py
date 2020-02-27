a9b6b0d49a00042982a4b627567eb152b9fd6aff
SHIRALI POUR Amir <amir.shirali-pour@etu.umontpellier.fr>
2020-02-27 12:54:43 +0100

    Remove guillemet

--- library/extraction.py
+++ library/extraction.py
@@ -34,6 +34,9 @@ def linksOfpagellapolitica():
                     div = table.find(
                         'div', {'class': 'mb-0 px-2 min-height-title'})
                     title = div.find('span', {'class': 'h6'}).text.strip()
+                    guillemetPattern = re.search(r'\\', title)
+                    if guillemetPattern:
+                        title = title[2:]
                 elif table.find('div', {'class': 'mb-2 mt-2 px-2'}):
                     div = table.find('div', {'class': 'mb-2 mt-2 px-2'})
 
@@ -49,15 +52,16 @@ def linksOfpagellapolitica():
                 link = table.find('a', {'class': 'statement-link'})['href']
                 idPattern = re.search(r'/([0-9]+)/', link)
                 idNumber = idPattern.group(1)
-                newArticles[idNumber] = {'link': link,
-                                         'title': title
-                                         }
+                newArticles[idNumber] = {
+                    'link': link,
+                    'title': title
+                }
                 print(totalNumberOfArticles, idNumber, title)
                 totalNumberOfArticles += 1
 
-                if idNumber == str(96):
+                if idNumber == str(215):
                     break
-        if idNumber == str(96):
+        if idNumber == str(215):
             break
 
     return newArticles
@@ -70,10 +74,10 @@ def writeLinksJson():
         json.dump(newArticles, jsonOut, indent=2, ensure_ascii=False)
 
 
-writeLinksJson()
-
-
 def readLinksJson():
     file = 'dataBase/newArticlesLinks.json'
     with open(file) as jsonIn:
         json.load(jsonIn)
+
+
+writeLinksJson()
